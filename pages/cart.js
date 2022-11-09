d8496a6bdb7e48b20ec2424102ef03f57c505160
Nyasha Musanhu01~ <danielsnyasha@outlook.com>
2022-11-09 16:24:52 +0200

    commit

--- pages/cart.js
+++ pages/cart.js
@@ -5,8 +5,9 @@ import Layout from "../components/Layout";
 import { Store } from "../utils/Store";
 import { XCircleIcon } from "@heroicons/react/outline";
 import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
 
-export default function CartScreen() {
+function CartScreen() {
   const router = useRouter;
   const { state, dispatch } = useContext(Store);
   const {
@@ -15,6 +16,10 @@ export default function CartScreen() {
 
   const removeItemHandler = (item) => {
     dispatch({ type: "CART_REMOVE_ITEM", payload: item });
+  };
+  const updateCartHandler = (item, qty) => {
+    const quantity = Number(qty);
+    dispatch({ type: "CART_ADD_ITEM", payload: { ...item, quantity } });
   };
   return (
     <Layout title='Shopping Cart'>
@@ -55,7 +60,20 @@ export default function CartScreen() {
                         </p>
                       </Link>
                     </td>
-                    <td className='p-5 text-right'>{item.quantity}</td>
+                    <td className='p-5 text-right'>
+                      <select
+                        value={item.quantity}
+                        onChange={(e) =>
+                          updateCartHandler(item, e.target.value)
+                        }
+                      >
+                        {[...Array(item.countInStock).keys()].map((x) => (
+                          <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                          </option>
+                        ))}
+                      </select>
+                    </td>
                     <td className='p-5 text-right'>R{item.price}</td>
                     <td className='p-5 text-center'>
                       <button onClick={() => removeItemHandler(item)}>
@@ -90,3 +108,4 @@ export default function CartScreen() {
     </Layout>
   );
 }
+export default dynamic(() => Promise.resolve(CartScreen), { ssr: false });
